3c17e899c458a1535c9e8e21a20505329e26d2ea
hx17 <hx17@illinois.edu>
2025-04-29 16:43:33 -0500

    tried adding rotation for realsense, maybe need to change info topic

--- realsense_adaptor/realsense_spencer_adaptor.py
+++ realsense_adaptor/realsense_spencer_adaptor.py
@@ -8,6 +8,9 @@ import cv_bridge
 import cv2
 from sensor_msgs.msg import Image
 
+CROP_W = 640
+CROP_H = 480
+
 def convertDepthEncoding(imageMsg, bridge):
     """
     convert given image from 16UC1 in mm to 32FC1 m
@@ -16,19 +19,40 @@ def convertDepthEncoding(imageMsg, bridge):
     """
     #convert from 16UC1 in mm to 32FC1 m
     cvImg = bridge.imgmsg_to_cv2(imageMsg)
-    cvImg32F = cvImg.astype('float32') / 1000.0
+    rotated_cvImg = cv2.rotate(cvImg, cv2.ROTATE_90_CLOCKWISE)
+    
+    h,w = rotated_cvImg.shape[:2]
+    w_start = (w-CROP_W) // 2
+    h_start = (h-CROP_H) // 2
+    cropped_cvImg = rotated_cvImg[h_start:h_start+CROP_H, w_start:w_start+CROP_W]
+
+    cvImg32F = cropped_cvImg.astype('float32') / 1000.0
     convertedImageMsg = bridge.cv2_to_imgmsg(cvImg32F)
     convertedImageMsg.header = imageMsg.header
     return convertedImageMsg
 
-def convertMonoEncoding(imageMsg):
+def convertMonoEncoding(imageMsg, bridge):
     """
     Resolves the error: "cv_bridge exception: [8UC1] is not a color format. but [bgr8] is."
     which arises when processing mono images from realsense using opencv.
     Note this is not required for using spencer tracking
     """
-    imageMsg.encoding = "mono8"
-    return imageMsg
+    cvImg = bridge.imgmsg_to_cv2(imageMsg)
+    rotated_cvImg = cv2.rotate(cvImg, cv2.ROTATE_90_CLOCKWISE)
+
+    h,w = rotated_cvImg.shape[:2]
+    w_start = (w-CROP_W) // 2
+    h_start = (h-CROP_H) // 2
+    cropped_cvImg = rotated_cvImg[h_start:h_start+CROP_H, w_start:w_start+CROP_W]
+    
+    if len(cropped_cvImg.shape) == 2:
+        encoding = 'mono8'
+    elif cropped_cvImg.shape[2] == 3:
+        encoding = 'bgr8' if imageMsg.encoding.lower() in ['bgr8', 'bgr'] else 'rgb8'
+    convertedImageMsg = bridge.cv2_to_imgmsg(cropped_cvImg, encoding=encoding)
+    convertedImageMsg.header = imageMsg.header
+    # convertedImageMsg.encoding = "mono8"
+    return convertedImageMsg
 
 
 def runAdaptor():
@@ -40,7 +64,7 @@ def runAdaptor():
     subDepth = rospy.Subscriber('depth/image_rect_raw', Image, callbackDepth)
 
     pubInfra = rospy.Publisher('rgb/image_rect_color', Image, queue_size=10)
-    callbackInfra = lambda imgMsg : pubInfra.publish(convertMonoEncoding(imgMsg))
+    callbackInfra = lambda imgMsg : pubInfra.publish(convertMonoEncoding(imgMsg, bridge))
     subInfra = rospy.Subscriber('color/image_raw', Image, callbackInfra)
 
     rospy.spin()
